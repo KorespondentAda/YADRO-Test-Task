e9d5e9e3ae742256e6b16a4beed4285e817a92ea
KorespondentAda <0kononov0@gmail.com>
2023-02-22 16:43:25 +0300

    Error handling, tests, beautify

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <charconv>
 #include <fstream>
 #include <sstream>
 #include <string>
 #include <vector>
 #include <map>
 
+// TODO Don't need, use just string?
 class Cell {
 private:
 	int value;
@@ -15,7 +17,7 @@ public:
 	Cell(std::string s) : str(s)
 	{}
 
-	bool isNeedToCalculate() { return !calculated; }
+	bool isCalculated() { return calculated; }
 	std::string getString() { return str; }
 	int getValue() { return value; }
 	void Calculate() {
@@ -23,61 +25,74 @@ public:
 };
 
 int main(int argc, char *argv[]) {
-	// TODO Get filename from argv
-	std::ifstream in("../file.csv");
-	// TODO Error: file does not open
+	if (argc < 2) {
+		std::cerr << "Usage: " << argv[0] << " <filename.csv>" << std::endl;
+		return 1;
+	}
+
+	char *filename = argv[1];
+
+	std::ifstream in(filename);
+	if (!in.is_open()) {
+		std::cerr << "Error: can't open file \"" << filename << "\"." << std::endl;
+		return 1;
+	}
 
 	int colCount = 0;
 	int row = 0, col = 0;
 	std::map<std::string, int> cols;
-	std::map<std::string, int> rows;
+	std::map<int, int> rows;
 	std::vector<std::vector<Cell>> table;
 
 	// Read cells to table
-	// TODO Maybe read all cells to table, then just throw names to maps?
 	for (std::string line; std::getline(in, line); row++, col = 0) {
-		// TODO Error while reading?
 		std::istringstream str(line);
 		std::vector<Cell> record;
 		for (std::string cell; std::getline(str, cell, ','); col++) {
-			// XXX
-			//std::cout << "Cell " << row << ':' << col << ": " << cell << std::endl;
 			record.push_back(cell);
 		}
 		if (colCount == 0) {
 			if (col == 0) {
-				// TODO Error: no header fields
+				std::cerr << "Error: no fields in first (header) record." << std::endl;
+				return 1;
 			}
 			colCount = col;
 		}
 		if (col != colCount) {
-			// TODO Error: invalid count of fields in record
+			std::cerr << "Error: invalid number of fields in " << row << " record; got " << col << ", " << colCount << " expected." << std::endl;
+			return 1;
 		}
 		table.push_back(record);
 	}
 
-	// Read table headers
-	for (int i = 0; i < table.size(); i++) {
-		rows.insert({table[i][0].getString(), i});
+	if (table.empty()) {
+		std::cerr << "Error: file \"" << filename << "\" empty" << std::endl;
+		return 1;
 	}
-	for (int i = 0; i < table[0].size(); i++) {
+
+	// Read table headers and row numbers
+	if (!table[0][0].getString().empty()) {
+		std::cerr << "Warning: corner header field is non-empty; ignoring." << std::endl;
+	}
+	for (int i = 1; i < table[0].size(); i++) {
+		if (table[0][i].getString().empty()) {
+			std::cerr << "Error: column " << i << " header is empty." << std::endl;
+			return 1;
+		}
 		cols.insert({table[0][i].getString(), i});
 	}
-
-	/*
-	// XXX Check name mappings
-	std::cout << std::endl << "Col names:" << std::endl;
-	for (auto head : cols) {
-		std::cout << head.first << " -> " << head.second << std::endl;
+	for (int i = 1; i < table.size(); i++) {
+		int rowNum;
+		std::string str = table[i][0].getString();
+		auto [ptr, ec] { std::from_chars(str.data(), str.data() + str.size(), rowNum) };
+		if (ec != std::errc()) {
+			std::cerr << "Error: row " << i << " number is invalid." << std::endl;
+			return 1;
+		}
+		rows.insert({rowNum, i});
 	}
-	std::cout << std::endl << "Row names:" << std::endl;
-	for (auto head : rows) {
-		std::cout << head.first << " -> " << head.second << std::endl;
-	}
-	*/
 
-	// XXX
-	//std::cout << std::endl << "Result table:" << std::endl;
+	// Write result table
 	for (int i = 0; i < table.size(); i++) {
 		for (int j = 0; j < table[i].size(); j++) {
 			if (j != 0) std::cout << ',';
